94daaa32ffbe0bf9c1f5ca98d2f4cfd7a7235533
varsey <eugeny.varseev@gmail.com>
2016-09-08 00:17:23 +0300

    first commit

---
+++ customTransportFoam.C
@@ -0,0 +1,136 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    scalarTransportFoam
+
+Description
+    Solves a transport equation for a passive scalar
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvIOoptionList.H"
+#include "simpleControl.H"
+#include "singlePhaseTransportModel.H"
+#include "RASModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+    #include "createFvOptions.H"
+
+    simpleControl simple(mesh);
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating scalar transport\n" << endl;
+
+    while (simple.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+	
+	#include "CourantNo.H"
+        #include "readTimeControls.H"
+        #include "setDeltaT.H"  //correct timestep
+
+        while (simple.correctNonOrthogonal())
+        {
+            solve
+            (
+                fvm::ddt(C)
+              + fvm::div(phi, C)
+              - fvm::laplacian(DC+turbulence->nut()/nu*0.01*DC, C)
+             ==
+                fvOptions(C)
+            );
+        }
+
+        while (simple.correctNonOrthogonal())
+        {
+            solve
+            (
+                fvm::ddt(Cp)
+              + fvm::div(phi, Cp)
+              - fvm::laplacian(DC+turbulence->nut()/nu*0.01*DC, Cp)
+			//- fvm::laplacian((kb*T/(scalar(3.14)*scalar(3.14)*rho*d*turbulence->nu()))+turbulence->nut()/0.7, Cp)
+             ==
+                fvOptions(Cp)
+            );
+        }
+
+	volScalarField Cs = A*pow(10,scalar(6.21)-B/T); //calculating Cs
+
+	
+	forAll(C,celli)
+	{
+		if(C[celli] > Cs[celli])	//is Cs>Ccurrent?
+			{
+			Cp[celli] = Cp[celli] + ( C[celli] - Cs[celli] ); //if yes, lets define precipitation from Ccurrent
+			C[celli] = Cs[celli];				  //C goes without precipitation
+			}	
+	}
+	
+	forAll(Cp,celli)
+	{
+		if( ( Cp[celli] > 0) & (C[celli] < Cs[celli]) )
+			{
+			C[celli] = C[celli] + Cp[celli];
+			Cp[celli] = 0;
+				
+				if (C[celli] > Cs[celli])
+					{
+						Cp[celli] = (C[celli] - Cs[celli]);	
+						C[celli] = Cs[celli];	
+					}						  
+			}	
+	}
+	
+
+	if(runTime.outputTime())
+	{
+		C.write();
+    	}
+
+//	Info<< "(kb*T/(scalar(3.14)*scalar(3.14)*d*rho*turbulence->nu())))\n" << endl;
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
